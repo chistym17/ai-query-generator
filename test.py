0a656936d5765bb2c1911cb2a7020e0a473031d9
chistym17 <mchisty31@gmail.com>
2025-03-28 20:50:54 +0600

    render url added

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 import requests
 import json
 
-BASE_URL = "http://127.0.0.1:8000"
+BASE_URL = "https://ai-query-generator.onrender.com"
 
 prompts = [
     "Show me all orders made by user with ID 1.",
